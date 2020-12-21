83be329c010c04768523545db6702be7b1a6f524
hiYuzu <pomelo.yuzu1997@gmail.com>
2020-12-21 15:57:09 +0800

    新增限值设置

--- app/src/main/java/com/hb712/gleak_android/adapter/SeriesLimitAdapter.java
+++ app/src/main/java/com/hb712/gleak_android/adapter/SeriesLimitAdapter.java
@@ -1,9 +1,97 @@
 package com.hb712.gleak_android.adapter;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.hb712.gleak_android.R;
+import com.hb712.gleak_android.entity.SeriesInfo;
+import com.hb712.gleak_android.entity.SeriesLimitInfo;
+
+import java.util.List;
+
 /**
  * @author hiYuzu
  * @version V1.0
  * @date 2020/12/21 14:57
  */
-public class SeriesLimitAdapter {
+public class SeriesLimitAdapter extends BaseAdapter {
+    private Context context;
+    private List<SeriesLimitInfo> seriesLimitInfoList;
+    private int selectIndex = -1;
+
+    public SeriesLimitAdapter(Context context, List<SeriesLimitInfo> seriesLimitInfoList) {
+        this.context = context;
+        this.seriesLimitInfoList = seriesLimitInfoList;
+    }
+
+    public void clear() {
+        seriesLimitInfoList.clear();
+    }
+
+    @Override
+    public int getCount() {
+        if (seriesLimitInfoList == null) {
+            return 0;
+        }
+        return seriesLimitInfoList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        if (seriesLimitInfoList != null && position < seriesLimitInfoList.size()) {
+            return seriesLimitInfoList.get(position);
+        }
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        if (seriesLimitInfoList != null && position < seriesLimitInfoList.size()) {
+            return seriesLimitInfoList.get(position).getId();
+        }
+        return 0;
+    }
+
+    @SuppressLint("InflateParams")
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        class ViewHolder {
+            public TextView seriesName;
+            public TextView limitValue;
+        }
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.series_info_content, null);
+            viewHolder = new ViewHolder();
+            viewHolder.seriesName = convertView.findViewById(R.id.seriesNameTv);
+            viewHolder.limitValue = convertView.findViewById(R.id.seriesLimitTv);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        SeriesLimitInfo seriesLimitInfo = seriesLimitInfoList.get(position);
+        if (seriesLimitInfo.getSeriesInfo() == null) {
+            viewHolder.seriesName.setText("");
+        } else {
+            viewHolder.seriesName.setText(seriesLimitInfo.getSeriesInfo().getSeriesName());
+        }
+        viewHolder.limitValue.setText(String.valueOf(seriesLimitInfo.getMaxValue()));
+        convertView.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        if (position == selectIndex) {
+            convertView.setBackgroundColor(context.getResources().getColor(R.color.ff80cbc4, null));
+            return convertView;
+        }
+        convertView.setBackgroundColor(Color.parseColor("#FFFFFF"));
+        return convertView;
+    }
+
+    public void setSelectItem(int position) {
+        selectIndex = position;
+    }
 }
--- app/src/main/java/com/hb712/gleak_android/fragment/LimitSettingFragment.java
+++ app/src/main/java/com/hb712/gleak_android/fragment/LimitSettingFragment.java
@@ -8,14 +8,129 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 
 import com.hb712.gleak_android.R;
+import com.hb712.gleak_android.adapter.SeriesLimitAdapter;
+import com.hb712.gleak_android.controller.SeriesInfoController;
+import com.hb712.gleak_android.dao.DBManager;
+import com.hb712.gleak_android.entity.SeriesInfo;
+import com.hb712.gleak_android.entity.SeriesLimitInfo;
+import com.hb712.gleak_android.util.LogUtil;
+import com.hb712.gleak_android.util.ToastUtil;
+
+import java.util.List;
 
 public class LimitSettingFragment extends Fragment {
+    private static final String TAG = LimitSettingFragment.class.getSimpleName();
+
+    private ListView seriesLimitList;
+    private Spinner seriesSettingSp;
+    private EditText limitValue;
+
+    private List<SeriesInfo> seriesInfoList;
+    private SeriesLimitAdapter seriesLimitAdapter;
+    private List<SeriesLimitInfo> seriesLimitInfoList;
+    private int selectIndex = -1;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_limit_setting, container, false);
+        View fragment = inflater.inflate(R.layout.fragment_limit_setting, container, false);
+        initView(fragment);
+        return fragment;
+    }
+
+    private void initView(View fragment) {
+        seriesInfoList = SeriesInfoController.getAll();
+        String[] seriesNames = new String[seriesInfoList.size()];
+        int i = 0;
+        for (SeriesInfo temp : seriesInfoList) {
+            seriesNames[i] = temp.getSeriesName();
+        }
+        seriesSettingSp = fragment.findViewById(R.id.seriesSettingSp);
+        SpinnerAdapter spinnerAdapter = new ArrayAdapter<>(getContext(), R.layout.item_spinner_normal, seriesNames);
+        seriesSettingSp.setAdapter(spinnerAdapter);
+        limitValue = fragment.findViewById(R.id.limitValue);
+        try {
+            seriesLimitInfoList = DBManager.getInstance().getReadableSession().getSeriesLimitInfoDao().loadAll();
+        } catch (Exception e) {
+            ToastUtil.toastWithoutLog("本地数据库发生错误！");
+            LogUtil.assertOut(TAG, e, "SeriesLimitInfoDao");
+        }
+        seriesLimitList = fragment.findViewById(R.id.seriesLimitList);
+        seriesLimitAdapter = new SeriesLimitAdapter(getContext(), seriesLimitInfoList);
+        seriesLimitList.setAdapter(seriesLimitAdapter);
+        seriesLimitList.setOnItemClickListener((parent, view, position, id) -> {
+            selectIndex = position;
+            seriesLimitAdapter.setSelectItem(position);
+            seriesLimitAdapter.notifyDataSetChanged();
+        });
+        Button add = fragment.findViewById(R.id.addNewLimitSetting);
+        add.setOnClickListener(v -> {
+            String limitValue = this.limitValue.getText().toString().trim();
+            if (limitValue.isEmpty()) {
+                ToastUtil.toastWithoutLog("请输入限值！");
+                return;
+            }
+            int position = seriesSettingSp.getSelectedItemPosition();
+            double d = Double.parseDouble(limitValue);
+            SeriesLimitInfo seriesLimitInfo = new SeriesLimitInfo();
+            seriesLimitInfo.setSeriesId(seriesInfoList.get(position).getId());
+            seriesLimitInfo.setSeriesInfo(seriesInfoList.get(position));
+            seriesLimitInfo.setMaxValue(d);
+            boolean isHave = false;
+            for (SeriesLimitInfo temp : seriesLimitInfoList) {
+                if (temp.getSeriesInfo().getId().equals(seriesLimitInfo.getSeriesId())) {
+                    temp.setMaxValue(seriesLimitInfo.getMaxValue());
+                    seriesLimitInfo.setId(temp.getId());
+                    isHave = true;
+                    break;
+                }
+            }
+            if (!isHave) {
+                seriesLimitInfoList.add(seriesLimitInfo);
+            }
+            selectIndex = -1;
+            try {
+                DBManager.getInstance().getWritableSession().getSeriesLimitInfoDao().save(seriesLimitInfo);
+            } catch (Exception e) {
+                ToastUtil.toastWithoutLog("本地数据库发生错误！");
+                LogUtil.assertOut(TAG, e, "SeriesInfoDao");
+            }
+            seriesLimitAdapter.setSelectItem(selectIndex);
+            seriesLimitAdapter.notifyDataSetChanged();
+        });
+        Button del = fragment.findViewById(R.id.deleteLimitSetting);
+        del.setOnClickListener(v -> {
+            if (selectIndex > -1) {
+                SeriesLimitInfo seriesLimitInfo = seriesLimitInfoList.get(selectIndex);
+                seriesInfoList.remove(selectIndex);
+                selectIndex = -1;
+                try {
+                    DBManager.getInstance().getWritableSession().getSeriesLimitInfoDao().delete(seriesLimitInfo);
+                } catch (Exception e) {
+                    ToastUtil.toastWithoutLog("本地数据库发生错误！");
+                    LogUtil.assertOut(TAG, e, "SeriesLimitInfoDao");
+                }
+                seriesLimitAdapter.setSelectItem(selectIndex);
+                seriesLimitAdapter.notifyDataSetChanged();
+                return;
+            }
+            ToastUtil.toastWithoutLog("请选择要删除的项目！");
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        seriesLimitAdapter.notifyDataSetChanged();
     }
 }
