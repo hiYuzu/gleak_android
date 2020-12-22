2976b5deb31c0594303a3fc435f9eb4f30e61982
hiYuzu <pomelo.yuzu1997@gmail.com>
2020-12-22 11:04:43 +0800

    修改显示时间为LONG

--- app/src/main/java/com/hb712/gleak_android/util/ToastUtil.java
+++ app/src/main/java/com/hb712/gleak_android/util/ToastUtil.java
@@ -6,7 +6,7 @@ import com.hb712.gleak_android.base.BaseApplication;
 
 public class ToastUtil {
     private static final String TAG = ToastUtil.class.getSimpleName();
-    private static final Toast mToast = Toast.makeText(BaseApplication.baseApplication, "", Toast.LENGTH_SHORT);
+    private static final Toast mToast = Toast.makeText(BaseApplication.baseApplication, "", Toast.LENGTH_LONG);
 
     public static void toastWithLog(CharSequence cs) {
         LogUtil.debugOut(TAG, cs.toString());
