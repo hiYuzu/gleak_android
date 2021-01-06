f2f6020c541816e966f241f7aae264e8860f8f63
hiYuzu <pomelo.yuzu1997@gmail.com>
2021-01-06 13:37:20 +0800

    去掉锁

--- app/src/main/java/com/hb712/gleak_android/util/ToastUtil.java
+++ app/src/main/java/com/hb712/gleak_android/util/ToastUtil.java
@@ -14,12 +14,12 @@ public class ToastUtil {
     private static final Toast M_LONG_TOAST = Toast.makeText(BaseApplication.baseApplication, "", Toast.LENGTH_LONG);
     private static final Toast M_SHORT_TOAST = Toast.makeText(BaseApplication.baseApplication, "", Toast.LENGTH_SHORT);
 
-    public static synchronized void longToastShow(CharSequence cs) {
+    public static void longToastShow(CharSequence cs) {
         M_LONG_TOAST.setText(cs);
         M_LONG_TOAST.show();
     }
 
-    public static synchronized void shortToastShow(CharSequence cs) {
+    public static void shortToastShow(CharSequence cs) {
         M_SHORT_TOAST.setText(cs);
         M_SHORT_TOAST.show();
     }
